8dd301560d90adbca739b9f027d1d976a97fb907
temporafugiunt <temporafugiunt@gmail.com>
2013-11-14 00:39:47 -0600

    Initial setup of base structure of IL PubData API solution. Added configuration helper, extension methods, record request and response filters to log to file locally (still working on DB server write), and the /Ping and /Config REST API commands. Setup base app host and IOC for SiteConfig and LogHelper.

---
+++ PD.API.Services/Helpers/SiteConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.Configuration;
+
+namespace PD.API.Services.Helpers
+{
+    public class SiteConfig
+    {
+        public string OAuthURL { get; private set; }
+        public string AppID { get; private set; }
+
+        public SiteConfig(IResourceManager resources)
+        {
+            OAuthURL = resources.GetString("OAuthURL");
+            if (OAuthURL == null)
+            {
+                ThrowConfigException("OAuthURL");
+            }
+
+            AppID = resources.GetString("AppID");
+            if (AppID == null)
+            {
+                ThrowConfigException("AppID");
+            }
+        }
+
+        private void ThrowConfigException(string configValue)
+        {
+            throw new ConfigurationErrorsException("'" + configValue + "' web config entry is not found.");
+        }
+    }
+}
