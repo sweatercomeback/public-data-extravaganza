828df7defb028375fd065a7786141d0981748920
temporafugiunt <temporafugiunt@gmail.com>
2013-11-13 22:01:43 -0600

    Committed changes. Blah blah blah Adam sucks balls.

---
+++ PD.API.Model/VersionInformationRequest.cs
@@ -0,0 +1,18 @@
+﻿using ServiceStack.ServiceHost;
+
+namespace PD.API.Model
+{
+    [Route("/VersionInformation", "GET, POST")]
+    public class VersionInformationRequest
+    {
+        public int MSTimeBeforeResponse { get; set; }
+#if DEBUG
+        public bool ThrowException { get; set; }
+#endif
+    }
+
+    public class VersionInformationResponse
+    {
+        public string FullVersion { get; set; }
+    }
+}
