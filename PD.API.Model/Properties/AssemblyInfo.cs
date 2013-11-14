c44512d4abdfab3a09f8578954d4ea7cc2e7eb69
temporafugiunt <temporafugiunt@gmail.com>
2013-11-13 21:36:52 -0600

    Added common version information file.

--- PD.API.Model/Properties/AssemblyInfo.cs
+++ PD.API.Model/Properties/AssemblyInfo.cs
@@ -8,29 +8,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PD.API.Model")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP")]
-[assembly: AssemblyProduct("PD.API.Model")]
-[assembly: AssemblyCopyright("Copyright © HP 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6aa2d226-e31e-4647-b35b-683752ed0a1c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
