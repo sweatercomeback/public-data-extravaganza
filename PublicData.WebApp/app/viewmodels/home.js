b6857ea2edc062e557366014bf34ded1d11e860e
CTumminaro <CTumminaro@gmail.com>
2013-11-14 23:09:52 -0600

    Ruining Adam's styles and shit

--- PublicData.WebApp/app/viewmodels/home.js
+++ PublicData.WebApp/app/viewmodels/home.js
@@ -1,6 +1,18 @@
 define(['plugins/router', 'durandal/app'], function (router, app) {
+    function initialize() {
+        var mapOptions = {
+            center: new google.maps.LatLng(42.273752, -89.066849),
+            zoom: 10,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
     return {
-    	activate: function () {
+        activate: function () {
+            
     	}
     }
 });
