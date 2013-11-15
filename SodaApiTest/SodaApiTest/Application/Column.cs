ee3df6044dc6847aaa9d9e15503c375ebf3f75ce
kek1205 <e.keum1205@gmail.com>
2013-11-14 22:50:09 -0600

    Add soda...

---
+++ SodaApiTest/SodaApiTest/Application/Column.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace SodaApiTest.Application
+{
+    public class Column
+    {
+        
+        public string name { get; set; }
+        
+        public string dataTypeName { get; set; }
+        
+        public string fieldName { get; set; }
+
+        public string fieldValue { get; set; }
+
+    }
+}
