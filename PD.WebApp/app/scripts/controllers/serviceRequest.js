c18cae5f4ee53f87dd8c55919d2854bf581401f1
CTumminaro <CTumminaro@gmail.com>
2013-11-16 13:37:46 -0600

    added contact and changed serviceRequest model

--- PD.WebApp/app/scripts/controllers/serviceRequest.js
+++ PD.WebApp/app/scripts/controllers/serviceRequest.js
@@ -15,31 +15,64 @@ speakUpApp.controller('EventController',
                 {
                     name: 'Pothole on Alpine and Spring Creek',
                     category: 'Patch Pothole',
+                    description: 'This pothole gets me every time! It really puts your suspension to the test!',
                     location: {
-                        address: 'Google Headquarters',
-                        city: 'Mountain View',
-                        province: 'CA'
+                        address: '634 Willoughby Avenue',
+                        city: 'Rockford',
+                        province: 'IL',
+                        "position": {
+                            "lat": 42.299786,
+                            "long": -89.070303,
+                        },
+                        "pov": {
+                            "heading": 172.35616438356166,
+                            "pitch": -45,
+                        },
+                        "panoID": "LRGombGStGdjMElWvZfeiw"
                     },
                     upVoteCount: 0
                 },
                 {
                     name: 'Can\'t see stop sign!',
                     category: 'Visibility Issues',
+                    description: 'Stop sign is not visible due to push pertruding into street.',
                     location: {
-                        address: 'Google Headquarters',
-                        city: 'Mountain View',
-                        province: 'CA'
+                        address: 'Hancock St & Cottage Grove Ave',
+                        city: 'Rockford',
+                        province: 'IL',
+                        "position": {
+                            "lat": 42.292977,
+                            "long": -89.07255399999997,
+                        },
+                        "pov": {
+                            "heading": 337.56164383561645,
+                            "pitch": -45,
+                        },
+                        "panoID": "QmE5T51XhbY_x-bfBYoEQA"
                     },
                     upVoteCount: 0
                 },
                 {
                     name: 'Gang Graffiti',
                     category: 'Graffiti',
+                    description: 'Graffiti is gang related and should be removed.',
                     location: {
-                        address: 'Google Headquarters',
-                        city: 'Mountain View',
-                        province: 'CA'
+                        address: '605 Fulton Avenue',
+                        city: 'Rockford',
+                        province: 'IL',
+                        "position": {
+                            "lat": 42.298572,
+                            "long": -89.07119999999998,
+                        },
+                        "pov": {
+                            "heading": -111.94520547945204,
+                            "pitch": -35.13698630136986,
+                        },
+                        "panoID": "M8ua-t-r5AvypIMxuQKraw"
                     },
+                    images: [
+                        { url: '/uploads/test.jpg'}
+                    ],
                     upVoteCount: 0
                 }
             ]
