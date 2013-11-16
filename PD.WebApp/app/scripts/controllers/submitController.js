5712faf632bbdfdc70d52ef17e4deb747ce83096
Adam Tumminaro <adam@tumminaro.com>
2013-11-16 14:50:04 -0600

    submit stuff

--- PD.WebApp/app/scripts/controllers/submitController.js
+++ PD.WebApp/app/scripts/controllers/submitController.js
@@ -1,12 +1,28 @@
 'use strict';
 
 speakUpApp.controller('SubmitController', function ($scope, dataRepository) {
-  	 $scope.address = '';
+  	 angular.extend($scope, {
+        centerProperty: {
+            lat: 45,
+            lng: -73
+        },
+        zoomProperty: 8,
+        markersProperty: [ {
+                latitude: 45,
+                longitude: -74
+            }],
+        clickedLatitudeProperty: null,  
+        clickedLongitudeProperty: null,
+    });
+
+  	 $scope.centerProperty = 
+  	 $scope.name = '';
   	 $scope.serviceCategory = '';
   	 $scope.description = '';
 
   	 $scope.serviceCategories = dataRepository.fetchServiceCategories();
 
+  	 
 
   	$scope.submit = function() {
   		console.log($scope.serviceCategory);
