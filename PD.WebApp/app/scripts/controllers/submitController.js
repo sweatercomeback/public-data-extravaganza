2377248a866f2fa999a1fe0c811f587fc756a15f
Adam Tumminaro <adam@tumminaro.com>
2013-11-16 15:39:52 -0600

    latlong updating

--- PD.WebApp/app/scripts/controllers/submitController.js
+++ PD.WebApp/app/scripts/controllers/submitController.js
@@ -1,25 +1,14 @@
 'use strict';
 
 speakUpApp.controller('SubmitController', function ($scope, dataRepository) {
-  	 angular.extend($scope, {
-        centerProperty: {
-            lat: 45,
-            lng: -73
-        },
-        zoomProperty: 8,
-        markersProperty: [ {
-                latitude: 45,
-                longitude: -74
-            }],
-        clickedLatitudeProperty: null,  
-        clickedLongitudeProperty: null,
-    });
+  	
 
   	 $scope.centerProperty = 
   	 $scope.name = '';
   	 $scope.serviceCategory = '';
   	 $scope.description = '';
-
+  	 $scope.latitude = '';
+  	 $scope.longitude = '';
   	 $scope.serviceCategories = dataRepository.fetchServiceCategories();
 
   	 
@@ -32,8 +21,8 @@ speakUpApp.controller('SubmitController', function ($scope, dataRepository) {
 
 
 speakUpApp.directive('initGoogleMaps', function () {
-    return function (scope, element, attrs) {
-        scope.$watch(attrs.initGoogleMaps, function () {
+    return function ($scope, element, attrs) {
+        $scope.$watch(attrs.initGoogleMaps, function () {
             //  Google Maps
             var mapOptions = {
               center: new google.maps.LatLng(42.270005, -89.096203),
@@ -43,7 +32,12 @@ speakUpApp.directive('initGoogleMaps', function () {
             var map = new google.maps.Map(element.get(0), mapOptions);
 
             google.maps.event.addListener(map, 'click', function( event ){
-              alert( "Latitude: "+event.latLng.lat()+" "+", longitude: "+event.latLng.lng() ); 
+              $scope.latitude = event.latLng.lat();
+              $scope.longitude = event.latLng.lng();
+
+              $scope.$apply();
+              //alert( "Latitude: "+event.latLng.lat()+" "+", longitude: "+event.latLng.lng() ); 
+
             });
 
         });
