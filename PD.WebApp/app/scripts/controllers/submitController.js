4c00ed2b7333fdcb6e52eba3d724baaec53eec80
CTumminaro <CTumminaro@gmail.com>
2013-11-16 15:10:00 -0600

    google maps added, fixed json mock data

--- PD.WebApp/app/scripts/controllers/submitController.js
+++ PD.WebApp/app/scripts/controllers/submitController.js
@@ -29,3 +29,23 @@ speakUpApp.controller('SubmitController', function ($scope, dataRepository) {
   	}
 
   });
+
+
+speakUpApp.directive('initGoogleMaps', function () {
+    return function (scope, element, attrs) {
+        scope.$watch(attrs.initGoogleMaps, function () {
+            //  Google Maps
+            var mapOptions = {
+              center: new google.maps.LatLng(42.270005, -89.096203),
+              zoom: 14,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(element.get(0), mapOptions);
+
+            google.maps.event.addListener(map, 'click', function( event ){
+              alert( "Latitude: "+event.latLng.lat()+" "+", longitude: "+event.latLng.lng() ); 
+            });
+
+        });
+    }
+});
